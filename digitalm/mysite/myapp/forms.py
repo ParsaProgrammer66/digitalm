51f9cfbec978607740df8a524b9ff5a0e591bcb3
parsaprogrammer2 <ParsaPR66@gmail.com>
2024-01-28 17:39:16 +0330

    digitalm

---
+++ digitalm/mysite/myapp/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Product
+from django.contrib.auth.models import User
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model=Product
+        fields=['name','description','price','file']
+
+
+class UserRegisterationForm(forms.ModelForm):
+    password=forms.CharField(label="password",widget=forms.PasswordInput)
+    password2=forms.CharField(label="Confirm password",widget=forms.PasswordInput)
+
+    class Meta:
+        model=User
+        fields=['username','email','first_name']
+        
+    def check_password(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError('password fields do not match')
+        return self.cleaned_data['password2']
